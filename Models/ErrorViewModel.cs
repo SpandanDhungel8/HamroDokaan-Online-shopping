54d3c7864e9d079a1adaaa3cd2c230d884fd7bf1
SpandanDhungel8 <spandandhungel8@gmail.com>
2026-06-24 18:25:26 +0545

    Initial Work

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HamroDokaan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
